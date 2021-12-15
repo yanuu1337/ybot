37b243bc2551cb7aaf58d839d2a955ef078f77f5
yanuu1337 <yanuu.why@gmail.com>
2021-12-15 10:07:35 +0100

    some changes

--- src/commands/admin/testing.ts
+++ src/commands/admin/testing.ts
@@ -1,3 +1,4 @@
+import { Collection } from 'discord.js';
 import { Message } from 'discord.js';
 import ArosClient from "../../extensions/ArosClient";
 import Command from "../../lib/structures/Command";
@@ -7,6 +8,6 @@ export default class extends Command {
     async execute(client: ArosClient, message: Message, args: string[]) {
         message.reply({content: client.translate.get(`${args[0] ?? 'en-US'}`)?.(`misc:PING`, {ping: client.ws.ping})})
         
-        
+       
     }
 }
