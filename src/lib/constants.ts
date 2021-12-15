19caf7988b9d164c518fce69f3277284807f84de
yanuu1337 <yanuu.why@gmail.com>
2021-12-15 09:58:31 +0100

    feat(const): all languages

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const availableLanguages = [
+    {
+        name: 'en-US',
+        fullName: 'English',
+        complete: true,
+        aliases: ['us', 'american', 'usa', 'english']
+    },
+    {
+        name: 'pl-PL',
+        fullName: 'Polish',
+        complete: true,
+        aliases: ['pl', 'polish', 'poland', 'polska', 'polski']
+    }
+]
