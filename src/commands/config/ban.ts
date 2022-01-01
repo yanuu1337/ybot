fdc710c6d5652996257327c9e7f82b35052fe898
yanuu1337 <yanuu.why@gmail.com>
2022-01-01 20:15:24 +0100

    feat: ban

---
+++ src/commands/config/ban.ts
@@ -0,0 +1,53 @@
+import { Message, PermissionString } from "discord.js";
+import ArosClient from "../../extensions/ArosClient";
+import Command from "../../lib/structures/Command";
+import { GuildInterface } from "../../lib/types/database";
+import EmbedFactory from "../../util/EmbedFactory";
+import Utility from "../../util/Utility";
+
+export default class extends Command {
+    permissions = ["BAN_MEMBERS"] as PermissionString[];
+    botPermissions = ["BAN_MEMBERS"] as PermissionString[];
+    async execute(client: ArosClient, message: Message<boolean>, args: string[], guild: GuildInterface | null): Promise<any> {
+        //ban a member, register the ban in guild cache
+        const member = message.mentions.members?.first() || message.guild?.members.cache.get(args[0]) || await message.guild?.members.fetch(args[0]).catch(err => null);
+        if(!args[0] || !member) {
+            return message.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/ban:INVALID_MEMBER")}`)
+                ]}
+            )
+        }
+
+        let reason = "None";
+        if(!args[1]) reason = Utility.translate(guild?.language, "mod/ban:NO_REASON");
+        else reason = args.slice(1).join(" ");
+
+        if(!member.bannable) {
+            return message.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/ban:MEMBER_NOT_BANNABLE")}`)
+                ]}
+            )
+        }
+        const modLogEmbed = EmbedFactory.generateModerationEmbed(message.author, member, "ban", reason);
+        const banEmbed = EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "mod/ban:BAN_SUCCESS", {member: member.user.tag, reason: reason, guild: message?.guild?.name})}`);
+        
+        await member.ban({reason: reason}).catch(err => {
+            client.logger.error(err);
+            return message.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/ban:BAN_FAILED")}`)
+                ]}
+            )
+        });
+        message.reply({embeds: [banEmbed]});
+
+
+        const modLogChannel = guild?.mod_log ? message.guild?.channels.cache.get(guild?.mod_log) : null; 
+        if(!modLogChannel?.isText()) return;
+        await modLogChannel?.send({embeds: [modLogEmbed]}).catch(err => {
+            return message.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/common:MOD_LOG_FAILED")}`)
+                ]}
+            )
+        });
+    }
+}
