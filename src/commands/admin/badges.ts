86941d3b25a3cd7e1519af6d2d368c276660eca7
yanuu1337 <yanuu.why@gmail.com>
2022-01-02 23:34:12 +0100

    feat: user badges

---
+++ src/commands/admin/badges.ts
@@ -0,0 +1,44 @@
+import { Badges } from './../../lib/types/database';
+import { Message } from "discord.js";
+import ArosClient from "../../extensions/ArosClient";
+import Command from "../../lib/structures/Command";
+import { BadgeEmojis, GuildInterface } from "../../lib/types/database";
+
+export default class extends Command {
+    category = 'admin';
+    name = 'badge';
+    aliases = ['badges', 'devbg', 'bgs', 'bdg'];
+    dm = true;
+    description = 'Grant a user a badge.';
+    async execute(client: ArosClient, message: Message<boolean>, args: string[], guild: GuildInterface | null): Promise<any> {
+        
+        if(!args[0] || !Object.keys(BadgeEmojis).includes(args[0].toUpperCase())) {
+            return message.reply(`Please specify a valid badge.`);
+        }
+        if(!message.mentions.members?.size && !args[1]) {
+            return message.reply(`Please specify a user.`);
+        }
+        const member = message.mentions.members?.first() ?? message.guild?.members.cache.get(args[1].toLowerCase()) ?? null;
+        console.log(member)
+        const badge = args[0].toUpperCase() as Badges;
+        const user = await client.handlers.users.fetch(member!.id)
+        if(!member) {
+            return message.reply(`Please specify a user.`);
+        }
+        if(!user) {
+            return message.reply(`That user is not registered.`)
+        }
+        if(user.badges?.some(b => b.name === badge)) {
+            const newBadges = user.badges.filter(b => b.name !== badge)
+            await client.handlers.users.edit(member?.id, {badges: JSON.stringify(newBadges)})
+            return message.reply(`Successfully removed that badge from the user. Current badges: ${newBadges.map(b => b.emoji).join(' ')}`)
+            
+        }
+        
+        
+        console.log(user)
+        const userBadges = [...(user.badges ?? []), {name: badge, emoji: BadgeEmojis[badge]}]
+        await client.handlers.users.edit(member?.id, {badges: JSON.stringify(userBadges)})
+        message.reply(`Successfully granted the user that badge. Current badges: ${userBadges.map(b => b.emoji).join(' ')}`)
+    }
+}
--- src/lib/types/database.ts
+++ src/lib/types/database.ts
@@ -5,7 +5,7 @@ export interface User {
     created_at?: string,
     blacklisted?: boolean,
     language?: string,
-
+    badges?: BadgesObject[] | null;
 } 
 export interface GuildInterface  {
     discord_id: string,
@@ -21,5 +21,15 @@ export interface Autoroles {
     members?: string | null;
     active?: boolean;
 }
-
+export type Badges = 'STAFF' | 'FRIEND' | 'PREMIUM' | 'TRUSTED_USER'
+export const BadgeEmojis = {
+    STAFF:`<:staff:927258825659121715>`,
+    PREMIUM: `<:premium:927259468004196413>`,
+    TRUSTED_USER: `<:trusted:927290620370882581>`,
+    FRIEND: `<:friendship:927291555591635005>`,
+} 
+export type BadgesObject = {
+    name: Badges,
+    emoji: string
+}
 export type MySQLResponseType = RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader | FieldPacket[];
