e044a43e09d33f6fda2f21d648a0322b5b261db7
yanuu1337 <yanuu.why@gmail.com>
2022-01-02 23:33:33 +0100

    fix(config): add current value view functionality

--- src/commands/config/config.ts
+++ src/commands/config/config.ts
@@ -34,30 +34,54 @@ export default class extends Command {
         if(!["language", "autorole", "modlog"].includes(args[0])) {
             return message.reply(Utility.translate(guild?.language, "config/cfg:INVALID_ARG"));
         }
-        //check if args[1] is a valid value
+        const argument = args[0].replace('modlog', 'mod_log') as keyof GuildInterface;
+
+        if(args[0] && !args[1]) {
+            return message.reply({embeds: [
+                EmbedFactory.generateInfoEmbed(
+                    `${Utility.translate(
+                        guild?.language,
+                        "common:SUCCESS"
+                    )}`,
+                    `${Utility.translate(
+                        guild?.language,
+                        "config/cfg:CURRENT_VALUE",
+                        {key: args[0], value: guild?.[argument]}
+                    )}`)
+            ]})
+        }
+
         if(args[0] === "language") {
             if(!["en", "pl"].includes(args[1])) {
                 return message.reply(Utility.translate(guild?.language, "config/cfg:INVALID_LANGUAGE"));
             }
             await client.handlers.guilds.edit(message.guild!, {language: args[1]});
             return message.reply({embeds: [EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "config/cfg:LANGUAGE_CHANGED", {lang: args[1]})}`)]});
+        
         } else if(args[0] === "autorole") {
+            
             if(!["true", "false"].includes(args[1])) {
                 return message.reply(Utility.translate(guild?.language, "config/cfg:INVALID_ARG"));
             }
             await client.handlers.guilds.edit(message.guild!, {autoroles: {active: args[1] === "true"}});
             return message.reply({embeds: [EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "config/cfg:AUTOROLE_CHANGED", {active: args[1] === "true" ? Utility.translate(guild?.language, "common:YES") : Utility.translate(guild?.language, "common:NO")})}`)]});
+        
         } else if(args[0] === "modlog") {
+            
             if(!args[1] || args[1] === "none" || args[1] === "null") {
                 await client.handlers.guilds.edit(message.guild!, {mod_log: null});
                 return message.reply({embeds: [EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "config/cfg:MODLOG_CHANGED", {channel: Utility.translate(guild?.language, "common:NONE")})}`)]});
             }
+            
             const channel = message.guild?.channels.cache.find(c => c.id === args[1]);
+            
             if(!channel) {
                 return message.reply(Utility.translate(guild?.language, "config/cfg:INVALID_CHANNEL"));
             }
+            
             await client.handlers.guilds.edit(message.guild!, {mod_log: args[1]});
             return message.reply({embeds: [EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "config/cfg:MODLOG_CHANGED", {channel: channel.toString()})}`)]});
+        
         } else {
             return message.reply(Utility.translate(guild?.language, "config/cfg:INVALID_ARG"));
         }
