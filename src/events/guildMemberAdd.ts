de10e604e911f9a0fb0d4da4211148564753f31b
yanuu1337 <yanuu.why@gmail.com>
2022-01-26 11:49:05 +0100

    fixes: minor fixes

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -3,11 +3,11 @@ import ArosClient from '../extensions/ArosClient';
 import Event from '../lib/structures/Event'
 export default class extends Event {
     async execute(client: ArosClient, member: GuildMember) {
-        //check if the guild has a blacklist, if so, return
-        if(!client.handlers.guilds.has(member.guild.id)) return;
+        
+        if(!(await client.handlers.guilds.fetch(member.guild.id))) return;
         const guild = client.handlers.guilds.get(member.guild.id);
         if(guild?.blacklisted) return;
-        //check if the member is a bot, if so add a respective bot role
+        
         if(guild?.autoroles?.active) {
             if(member.user.bot) {
                 const botRole = member.guild.roles.cache.find(r => r.id === guild?.autoroles?.bots);
