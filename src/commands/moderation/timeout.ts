65cefddcdf33e4e9f2a8bfc2328d7eb095617ae1
yanuu1337 <yanuu.why@gmail.com>
2022-01-30 01:30:40 +0100

    feat: slash commands for timeout/untimeout

--- src/commands/moderation/timeout.ts
+++ src/commands/moderation/timeout.ts
@@ -1,4 +1,5 @@
-import { TextChannel } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CacheType, CommandInteraction, TextChannel, GuildMember } from 'discord.js';
 import { Message, PermissionString } from "discord.js";
 import ArosClient from "../../extensions/ArosClient";
 import Command from "../../lib/structures/Command";
@@ -10,6 +11,11 @@ export default class extends Command {
     permissions = ["MODERATE_MEMBERS"] as PermissionString[];
     botPermissions = ["MODERATE_MEMBERS"] as PermissionString[];
     usage = 'timeout <user> <time> [reason]';
+    isSlashCommand = true;
+    data = new SlashCommandBuilder()
+        .addUserOption(opt => opt.setName("user").setDescription("The user to timeout").setRequired(true))
+        .addStringOption(opt => opt.setName("time").setDescription("The time to timeout the user for").setRequired(true))
+        .addStringOption(opt => opt.setName("reason").setDescription("The reason for the timeout").setRequired(false))
     description = 'Timeout a user for a certain amount of time';
     async execute(client: ArosClient, message: Message<boolean>, args: string[], guild: GuildInterface | null): Promise<any> {
         
@@ -54,6 +60,51 @@ export default class extends Command {
         const modLog = guild?.mod_log ? client.channels.cache.get(guild?.mod_log) as TextChannel : null;
         if(!modLog) return;
         modLog.send({embeds: [modLogEmbed]});
-    
+    }
+
+
+    async executeSlash(client: ArosClient, interaction: CommandInteraction<CacheType>, guild: GuildInterface | null, isInDms?: boolean): Promise<any> {
+        const member = interaction.options.getMember("user", true) as GuildMember;
+        const timeInMs = ms(interaction.options.getString("time", true));
+        if(member.communicationDisabledUntilTimestamp || member?.communicationDisabledUntilTimestamp! > Date?.now()) {
+            return interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:ALREADY_TIMEOUTED")}`)
+                ]}
+            )
+        }
+        if(!timeInMs) {
+            return interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:INVALID_TIME")}`)
+            ]})
+        }
+        
+        const reason = interaction.options.getString("reason") ?? Utility.translate(guild?.language, "common:NONE");
+
+        const modLogEmbed = EmbedFactory.generateModerationEmbed(interaction.user, member, "timeout", reason);
+        const timeoutEmbed = EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, 
+            `${Utility.translate(guild?.language, "mod/timeout:TIMEOUT_SUCCESS", {
+                member: member.user.tag,
+                time: interaction.options.getString("time"),
+                reason: reason
+        })}`);
+        
+        if(!member.moderatable) {
+            return interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:NOT_MODERATABLE")}`)
+            ]})
+        }
+        member.timeout(timeInMs, reason).then(() => {
+            interaction.reply({embeds: [timeoutEmbed]})
+        }).catch(err => {
+            client.logger.error(`GUILD: ${interaction?.guild?.id} | Error ocurred while timeouting: ${err}`, () => {}, err)
+            interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:TIMEOUT_ERROR")}`)
+            ]})
+        })
+
+        
+        const modLog = guild?.mod_log ? client.channels.cache.get(guild?.mod_log) as TextChannel : null;
+        if(!modLog) return;
+        modLog.send({embeds: [modLogEmbed]});
     }
 }
--- src/commands/moderation/untimeout.ts
+++ src/commands/moderation/untimeout.ts
@@ -1,4 +1,5 @@
-import { Message, PermissionString, TextChannel } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CacheType, CommandInteraction, GuildMember, Message, PermissionString, TextChannel } from "discord.js";
 import ArosClient from "../../extensions/ArosClient";
 import Command from "../../lib/structures/Command";
 import { GuildInterface } from "../../lib/types/database";
@@ -10,6 +11,11 @@ export default class extends Command {
     permissions = ['MODERATE_MEMBERS'] as PermissionString[]
     botPermissions = ['MODERATE_MEMBERS'] as PermissionString[]
     description = 'Untimeout a user';
+
+    isSlashCommand = true;
+    data = new SlashCommandBuilder()
+        .addUserOption(opt => opt.setName("user").setDescription("The user to untimeout").setRequired(true))
+        .addStringOption(opt => opt.setName("reason").setDescription("The reason for the untimeout").setRequired(false))
     async execute(client: ArosClient, message: Message<boolean>, args: string[], guild: GuildInterface | null): Promise<any> {
         
 
@@ -41,7 +47,7 @@ export default class extends Command {
         member.timeout(null, reason).then(() => {
             message.reply({embeds: [timeoutEmbed]})
         }).catch(err => {
-            client.logger.error(`GUILD: ${message?.guild?.id} | Error occured while trying to untimeout: ${err}`, () => {}, err);
+            client.logger.error(`GUILD: ${message?.guild?.id} | Error occurred while trying to untimeout: ${err}`, () => {}, err);
             message.reply({embeds: [
                 EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:TIMEOUT_ERROR")}`)
             ]})
@@ -49,7 +55,41 @@ export default class extends Command {
         const modLog = guild?.mod_log ? client.channels.cache.get(guild?.mod_log) as TextChannel : null;
         if(!modLog) return;
         modLog.send({embeds: [modLogEmbed]});
+    }
 
+
+    async executeSlash(client: ArosClient, interaction: CommandInteraction<CacheType>, guild: GuildInterface | null, isInDms?: boolean): Promise<any> {
+        const member = interaction.options.getMember("user", true) as GuildMember;
+        
+        if(!member.communicationDisabledUntilTimestamp || member?.communicationDisabledUntilTimestamp < Date?.now()) {
+            return interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:NOT_IN_TIMEOUT")}`)
+                ]}
+            )
+        }
+        if(!member.moderatable) {
+            return interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:NOT_MODERATABLE")}`)
+            ]})
+        }
+        const reason = interaction.options.getString("reason") ?? Utility.translate(guild?.language, "common:NONE");
+
+        const modLogEmbed = EmbedFactory.generateModerationEmbed(interaction.user, member, "untimeout", reason);
+        const timeoutEmbed = EmbedFactory.generateInfoEmbed(`${Utility.translate(guild?.language, "common:SUCCESS")}`, `${Utility.translate(guild?.language, "mod/timeout:UNTIMEOUT_SUCCESS", {member: member.user.tag, reason: reason})}`);
+
+
+
+        member.timeout(null, reason).then(() => {
+            interaction.reply({embeds: [timeoutEmbed]})
+        }).catch(err => {
+            client.logger.error(`GUILD: ${interaction?.guild?.id} | Error occurred while trying to untimeout: ${err}`, () => {}, err);
+            interaction.reply({embeds: [
+                EmbedFactory.generateErrorEmbed(`${Utility.translate(guild?.language, "common:ERROR")}`, `${Utility.translate(guild?.language, "mod/timeout:TIMEOUT_ERROR")}`)
+            ]})
+        })
+        const modLog = guild?.mod_log ? client.channels.cache.get(guild?.mod_log) as TextChannel : null;
+        if(!modLog) return;
+        modLog.send({embeds: [modLogEmbed]});
 
     }
 
